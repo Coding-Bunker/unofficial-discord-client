d5ceb2c9307416a36faefeb43237de16fba3978f
bond0O7 <bond0O7@github.com>
2020-08-12 15:27:02 +0200

    Inline The API Constants

--- DiscordAPI.hpp
+++ DiscordAPI.hpp
@@ -2,20 +2,18 @@
 
 #include <QString>
 
-namespace DiscordAPI
-{
-const QString baseURL      = "https://discord.com/api";
-const QString authorize    = baseURL + "/oauth2/authorize";
-const QString clientId     = "740334165651882036";
-const QString clientSecret = "og4kFEYYimJCaFn0v9R7mIPg45ciTRLL";
-const QString login        = baseURL + "/auth/login";
-const QString token        = baseURL + "/oauth2/token";
+namespace DiscordAPI {
+inline const QString baseURL = "https://discord.com/api";
+inline const QString authorize = baseURL + "/oauth2/authorize";
+inline const QString clientId = "740334165651882036";
+inline const QString clientSecret = "og4kFEYYimJCaFn0v9R7mIPg45ciTRLL";
+inline const QString login = baseURL + "/auth/login";
+inline const QString token = baseURL + "/oauth2/token";
 
-namespace DiscordTags
-{
-const QString access_token  = "access_token";
-const QString expires_in    = "expires_in";
-const QString refresh_token = "refresh_token";
+namespace DiscordTags {
+inline const QString access_token = "access_token";
+inline const QString expires_in = "expires_in";
+inline const QString refresh_token = "refresh_token";
 } // namespace DiscordTags
 
 } // namespace DiscordAPI
