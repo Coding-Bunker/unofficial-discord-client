b6e0dd18625600940b9ba2690ea23d8e876848ee
bond0O7 <bond0O7@github.com>
2020-08-14 20:46:49 +0200

    Move Login Function
    	I moved the login function to DiscordAPI.hpp file to preserve consistency

--- DiscordAPI.hpp
+++ DiscordAPI.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <QString>
+#include <QUrl>
+#include <QUrlQuery>
+#include <QDesktopServices>
 
 namespace DiscordAPI {
 inline const QString baseURL = "https://discord.com/api";
@@ -10,6 +13,19 @@ inline const QString clientSecret = "og4kFEYYimJCaFn0v9R7mIPg45ciTRLL";
 inline const QString login = baseURL + "/auth/login";
 inline const QString token = baseURL + "/oauth2/token";
 
+namespace Requests{
+inline void login()
+{
+    QUrl url(DiscordAPI::authorize);
+    QUrlQuery query;
+    query.addQueryItem("response_type", "code");
+    query.addQueryItem("client_id", DiscordAPI::clientId);
+    query.addQueryItem("scope", "identify%20email");
+    query.addQueryItem("redirect_uri", "http://localhost:8000");
+    url.setQuery(query);
+    QDesktopServices::openUrl(url);
+}
+} // namespacd Requests
 namespace DiscordTags {
 inline const QString access_token = "access_token";
 inline const QString expires_in = "expires_in";
--- MainWindow.cpp
+++ MainWindow.cpp
@@ -50,15 +50,7 @@ void MainWindow::aboutClicked()
 
 void MainWindow::requestLogin()
 {
-    // TODO(guerra): move this part in separated class
-    QUrl url(DiscordAPI::authorize);
-    QUrlQuery query;
-    query.addQueryItem("response_type", "code");
-    query.addQueryItem("client_id", DiscordAPI::clientId);
-    query.addQueryItem("scope", "identify%20email");
-    query.addQueryItem("redirect_uri", "http://localhost:8000");
-    url.setQuery(query);
-    QDesktopServices::openUrl(url);
+    DiscordAPI::Requests::login();
 }
 
 void MainWindow::newConnectionOnLocalServer()
