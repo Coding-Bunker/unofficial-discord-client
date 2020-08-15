3667a6a12dc99dccab827a357f8486fda6b4da12
bond0O7 <bond0O7@github.com>
2020-08-15 12:45:49 +0200

    move requests portion to a dedicated header file

--- DiscordApi/Requests.hpp
+++ DiscordApi/Requests.hpp
@@ -1,4 +1,23 @@
-#ifndef REQUESTS_HPP
-#define REQUESTS_HPP
+#pragma once
+#include "Constructs.hpp"
 
-#endif // REQUESTS_HPP
+#include <QUrl>
+#include <QUrlQuery>
+#include <QDesktopServices>
+
+namespace DiscordAPI{
+namespace Requests{
+inline void login()
+{
+    QUrl url(DiscordAPI::authorize);
+    QUrlQuery query;
+    query.addQueryItem("response_type", "code");
+    query.addQueryItem("client_id", DiscordAPI::clientId);
+    query.addQueryItem("scope", "identify%20email");
+    query.addQueryItem("redirect_uri", "http://localhost:8000");
+    url.setQuery(query);
+    QDesktopServices::openUrl(url);
+}
+
+} // namespace Requests
+} // namespace DiscordAPI
