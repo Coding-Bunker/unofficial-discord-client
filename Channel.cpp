9d21b2ebd18c5abb3324397ef03ea099b2b0e095
bond0O7 <bond0O7@github.com>
2020-08-18 17:03:19 +0200

    Added Channel struct 	I added Channel class composing of some datatypes mentioned in the discord API, I also added two de/serialize function from and to QJsonObject

--- Channel.cpp
+++
@@ -1,6 +0,0 @@
-#include "Channel.hpp"
-
-Channel::Channel()
-{
-
-}
--- Channel.hpp
+++ Channel.hpp
@@ -1,11 +1,44 @@
 #ifndef CHANNEL_HPP
 #define CHANNEL_HPP
+#include <QJsonObject>
+#include <QTime>
+#include <QUuid>
+#include <optional>
 
-
-class Channel
-{
+struct Channel {
   public:
-    Channel();
+    enum class Type {
+        GUILD_TEXT = 0,
+        DM,
+        GUILD_VOICE,
+        GROUP_DM,
+        GUILD_CATEGORY,
+        GUILD_NEWS,
+        GUILD_STORE
+    };
+
+  public:
+    QUuid id;
+    Type type;
+    QUuid guild_id;
+    int position;
+    // array of permissions overwrites
+    QString name;
+    QString topic;
+    bool nsfw = false;
+    QUuid last_message_id;
+    int bitrate;
+    unsigned int user_limit;
+    unsigned int rate_limit_per_user;
+    // reciptions of user objects
+    QString icons; // hash
+    QUuid owner_id;
+    QUuid application_id;
+    QUuid parent_id;
+    QTime last_pin_timestamp; // converted to ISO8601 standard
 };
+
+[[nodiscard]] inline Channel fromJsonToChannel(const QJsonObject &json_ojb);
+[[nodiscard]] inline QJsonObject fromChannelToJson(const Channel &channel);
 
 #endif // CHANNEL_HPP
