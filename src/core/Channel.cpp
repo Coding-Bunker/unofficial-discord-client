62291a21c18fd4242cc91f6fa1d601722bd3be57
Federico Guerinoni <guerinoni.federico@gmail.com>
2020-11-28 10:13:57 +0100

    Channel: Decode type
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/core/Channel.cpp
+++ src/core/Channel.cpp
@@ -8,6 +8,7 @@ void Channel::unmarshal(const QJsonObject &obj)
     m_parentId = obj.value("parent_id").toString().toULongLong();
     m_guildId  = obj.value("guild_id").toString().toULongLong();
     m_position = obj.value("position").toInt();
+    m_type     = static_cast<Type>(obj.value("type").toInt());
 }
 
 QString Channel::name() const
--- src/core/Channel.hpp
+++ src/core/Channel.hpp
@@ -20,5 +20,19 @@ class Channel
     snowflake m_guildId;
     snowflake m_parentId;
     int m_position;
-    // TODO: implement type of channel before add
+
+    enum Type {
+        GUILD_TEXT,     // a text channel within a server
+        DM,             // a direct message between users
+        GUILD_VOICE,    // a voice channel within a server
+        GROUP_DM,       // a direct message between multiple users
+        GUILD_CATEGORY, // an organizational category that contains up to 50
+                        // channels
+        GUILD_NEWS,  // a channel that users can follow and crosspost into their
+                     // own server
+        GUILD_STORE, // a channel in which game developers can sell their game
+                     // on Discord
+    };
+
+    Type m_type;
 };
