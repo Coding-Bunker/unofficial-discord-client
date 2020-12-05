3d9ddba2657c6799146e1821103bc7d761a575d3
Federico Guerinoni <guerinoni.federico@gmail.com>
2020-12-06 00:05:12 +0100

    Add ChannelsModel
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

---
+++ src/gui/ChannelsModel.cpp
@@ -0,0 +1,41 @@
+#include "ChannelsModel.hpp"
+
+ChannelsModel::ChannelsModel(const QList<Channel> &c, QObject *parent) :
+    QAbstractListModel(parent), m_channels{ c }
+{
+}
+
+int ChannelsModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return m_channels.size();
+}
+
+QVariant ChannelsModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid()) {
+        return {};
+    }
+
+    if (role == Role::Name) {
+        return m_channels.at(index.row()).name();
+    }
+
+    return {};
+}
+
+QHash<int, QByteArray> ChannelsModel::roleNames() const
+{
+    return { { Role::Name, "nameRole" } };
+}
+
+void ChannelsModel::select(int index)
+{
+    m_selected = index;
+    emit selectedChanged();
+}
+
+int ChannelsModel::selected() const
+{
+    return m_selected;
+}
---
+++ src/gui/ChannelsModel.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "core/Channel.hpp"
+
+#include <QAbstractListModel>
+
+class ChannelsModel : public QAbstractListModel
+{
+    // clang-format off
+    Q_OBJECT
+    Q_PROPERTY(int selected READ selected NOTIFY selectedChanged)
+    // clang-format on
+
+  public:
+    explicit ChannelsModel(const QList<Channel> &c, QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index,
+                  int role = Qt::DisplayRole) const override;
+
+    enum Role { Name = Qt::UserRole + 1 };
+
+    QHash<int, QByteArray> roleNames() const override;
+
+    Q_INVOKABLE void select(int index);
+
+    int selected() const;
+
+  signals:
+    void selectedChanged();
+
+  private:
+    const QList<Channel> &m_channels;
+    int m_selected;
+};
